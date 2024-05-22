23c07a7af3ee170e65aecbdb184e81e8e6984d6b
ccrawford4 <calumcrawford9@gmail.com>
2024-05-22 09:44:25 -0700

    Initial commit

---
+++ langlet/langlet/Views/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeView.swift
+//  langlet
+//
+//  Created by Calum Crawford on 5/22/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
+
---
+++ langlet/langlet/langletApp.swift
@@ -0,0 +1,19 @@
+//
+//  langletApp.swift
+//  langlet
+//
+//  Created by Calum Crawford on 5/21/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct langletApp: App {
+    // Put global state objects here
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
